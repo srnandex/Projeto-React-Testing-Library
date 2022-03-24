9352af3b1fff6597a16043c873c0762a0eb795d4
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-03-24 14:56:07 -0300

    teste pra ver se passa o requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
+describe('1. Teste o componente <App.js />', () => {
+  it('Teste se o topo da aplicação contém um conjunto de links de navegação.', () => {
+    renderWithRouter(<App />);
+    const liHome = screen.getByRole('link', { name: 'Home' });
+    expect(liHome).toBeDefined();
+    const liAbout = screen.getByRole('link', { name: 'About' });
+    expect(liAbout).toBeDefined();
+    const liFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(liFavorite).toBeDefined();
+  });
+});

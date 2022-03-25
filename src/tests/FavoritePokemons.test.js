39e5b4dddaa646afd58f10f4462f16380e0f8ea0
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-03-25 14:03:32 -0300

    requisito 5

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -3,12 +3,12 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from './helpers/renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
 
-describe('4. Teste o componente <FavoritePokemons.js />', () => {
+describe('3. Teste o componente <FavoritePokemons.js />', () => {
   beforeEach(() => {
     renderWithRouter(<FavoritePokemons />);
   });
   it('Teste se é exibido na tela a mensagem No favorite pokemon found.', () => {
-    const notFound = screen.getByText('No favorite pokemon found');
-    expect(notFound).toBeInTheDocument();
+    const notPokemon = screen.getByText('No favorite pokemon found');
+    expect(notPokemon).toBeInTheDocument();
   });
 });
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,47 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
+describe('5. Teste o componente <Pokedex.js />', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
+    const notPokemon = screen.getByText('Encountered pokémons');
+    expect(notPokemon).toBeInTheDocument();
+  });
+
+  it('Teste se página contém um h2 com o texto Encountered pokémons.', async () => {
+    const btnAll = screen.getByRole('button', { name: 'All' });
+    expect(btnAll).toBeDefined();
+
+    userEvent.click(btnAll);
+
+    const nameFirstPokemonAll = await screen.getByText('Pikachu');
+    expect(nameFirstPokemonAll).toBeInTheDocument();
+  });
+
+  it('Teste se página contém um h2 com o texto Encountered pokémons.', async () => {
+    const nameFirstPokemon = screen.getByText('Pikachu');
+    expect(nameFirstPokemon).toBeInTheDocument();
+
+    const btnNextpokemon = screen.getByRole('button', { name: 'Próximo pokémon' });
+    expect(btnNextpokemon).toBeDefined();
+
+    userEvent.click(btnNextpokemon);
+
+    const nameSecondPokemon = await screen.getByText('Charmander');
+    expect(nameSecondPokemon).toBeInTheDocument();
+  });
+
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
+    const magicNumber = 7;
+    const testId = screen.getAllByTestId('pokemon-type-button');
+    expect(testId.length).toEqual(magicNumber);
+
+    const typeEletric = screen.getByRole('button', { name: 'Electric' });
+    expect(typeEletric).toBeDefined();
+  });
+});

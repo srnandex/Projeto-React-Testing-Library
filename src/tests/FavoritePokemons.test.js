4d8b19046b7b82264c75c7b0e6bfbf85dfa537e5
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-03-25 13:11:53 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './helpers/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('4. Teste o componente <FavoritePokemons.js />', () => {
+  beforeEach(() => {
+    renderWithRouter(<FavoritePokemons />);
+  });
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found.', () => {
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+});
